4dac7f2b14a0275b706da4c22bd5bd0884d2dde7
L-A-Davis <lindsay.amanda.davis@gmail.com>
2017-11-08 15:55:48 -0500

    completed manipulating hashes methods

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,11 @@ def first_challenge
 
   #your code here
 
-
+contacts["Freddy Mercury"].each do |key, value|
+    if key == :favorite_icecream_flavors
+        value.delete ("strawberry")
+        end
+    end
   #remember to return your newly altered contacts hash!
   contacts
 end
